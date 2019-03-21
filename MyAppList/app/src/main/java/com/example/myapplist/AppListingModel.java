bc3d1124617fdecda3250a99255ebd4e87c0d5eb
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-21 14:38:30 -0500

    added data members

---
+++ MyAppList/app/src/main/java/com/example/myapplist/AppListingModel.java
@@ -0,0 +1,12 @@
+package com.example.myapplist;
+
+public class AppListingModel {
+    //app_version,
+
+    private int id, appVersion;
+    private String contactEmail, domainName, appName, imageUrl,
+
+    public AppListingModel(){
+
+    }
+}
