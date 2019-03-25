8c39ae245f5aee5ea79e380c7c6fc062397ad3ca
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-25 16:20:35 -0500

    if statment

---
+++ DailyProjects/Week 1/app/src/main/java/com/example/imageviewer2/ImageData.java
@@ -0,0 +1,37 @@
+package com.example.imageviewer2;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+public class ImageData implements Serializable {
+
+    private String imageData;
+    private String name;
+
+    public ImageData(String imageData, String name) {
+        this.imageData = imageData;
+        this.name = name;
+    }
+
+    public ImageData(String name) {
+        this.name = name;
+    }
+
+
+    public Uri getImageData() {
+        return Uri.parse(imageData);
+    }
+
+    public void setImageData(String imageData) {
+        this.imageData = imageData.toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
